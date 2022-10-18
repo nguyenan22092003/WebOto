c2a3815ffb94e9f6afcd57a5e8c5c69a73b2bc07
Nguyen Van An <an.nv.2030@aptechlearning.edu.vn>
2022-10-18 14:14:08 +0700

    Add project files.

---
+++ WebOto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebOto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
